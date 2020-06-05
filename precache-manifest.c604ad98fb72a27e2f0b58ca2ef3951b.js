3784444ce04750b2ba80cb037a13aa1ee999ccea
Kadievka <kadievka.s.ks@gmail.com>
2020-06-05 11:32:54 -0400

    Updates

---
+++ precache-manifest.c604ad98fb72a27e2f0b58ca2ef3951b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f28b7062c9dc20c5da5447ae8f48130",
+    "url": "/apple-home-react/index.html"
+  },
+  {
+    "revision": "776a1ff7ef2734d56de5",
+    "url": "/apple-home-react/static/css/main.7b35214d.chunk.css"
+  },
+  {
+    "revision": "aac308aa43e7a1c3916c",
+    "url": "/apple-home-react/static/js/2.e7161071.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/apple-home-react/static/js/2.e7161071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776a1ff7ef2734d56de5",
+    "url": "/apple-home-react/static/js/main.e56520fa.chunk.js"
+  },
+  {
+    "revision": "0af2b558a71f5dfbf125",
+    "url": "/apple-home-react/static/js/runtime-main.e693e4d0.js"
+  }
+]);
